de3a1783511d53ab02e9812025d4b2a9dc48d3cb
Fredolin <fbrone@redhat.com>
2025-06-06 16:57:36 +0530

    Delete Backend directory

--- Backend/pages/Notification.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from assistant import run_assistant
-
-if 'authenticated_user' in st.session_state:
-    run_assistant(st.session_state.authenticated_user['username'])
-else:
-    st.warning("Please log in to access the assistant.")
